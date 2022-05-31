063a1caf5297483dd14389b15aa10ffc69a8a007
Nutan Naik <nutann.naik@honeywell.com>
2022-05-31 12:52:04 +0530

    second commit

--- index.js
+++ index.js
@@ -4,18 +4,29 @@ const fs = require("fs");
 const express = require("express");
 
 const app = express();
-// app.listen(4000, () => {
-//     console.log('server is runing at pport 4000')
-// });
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+const server = http.createServer(app);
 
-http
-  .createServer(
+// http
+//   .createServer(
 
-    app
-  )
-  .listen(4000, () => {
-    console.log("serever is runing at port 4000");
+//     app
+//   )
+//   .listen(4000, () => {
+//     console.log("serever is runing at port 4000");
+//   });
+var port = process.env.PORT || 8090;
+server.listen(port,function (err) {
+    console.log("Listening to port "+port);
+});
+
+app.get('/',function(req,res){
+    console.log("******************");
+    res.send('We are happy to see you');
   });
-  app.get('/', (req,res)=>{
-    res.send("Hello from express server.")
-})
+  app.post('/',function (req,res) {
+
+    console.log("****************** POST request ***********");
+
+});
