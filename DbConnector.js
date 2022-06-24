7b2eebf486293ce3062c36c872fc76a6d7c28d24
Gaurav Kumar <gaurav.kumar3@honeywell.com>
2022-06-24 15:14:39 +0530

    Added redirection, db clean route and formatting

--- DbConnector.js
+++ DbConnector.js
@@ -8,60 +8,72 @@ function dbConnector() {
     function initializeDb() {
 
         console.log("initialize db *********");
-        
+
         pointDb.run("CREATE TABLE IF NOT EXISTS pointInfo (point_id Text PRIMARY KEY,point_name Text," +
-                    "outvalue TEXT)");
+            "outvalue TEXT)");
 
     }
 
-
-    function updateDb(point,callback) {
-        console.log("updating the db with details "+JSON.stringify(point));
+    function updateDb(point, callback) {
+        console.log("updating the db with details " + JSON.stringify(point));
         var cb = function (err) {
-            console.log("error occured" +err);
+            if (err) {
+                console.log("Error occured during updating database: " + err);
+            }
             callback(err);
         }
-        pointDb.run("INSERT into pointInfo(point_id,point_name,outvalue) VALUES (?,?,?)",point.point_id,point.point_name,point.outvalue,cb);
+        pointDb.run("INSERT into pointInfo(point_id,point_name,outvalue) VALUES (?,?,?)", point.point_id, point.point_name, point.outvalue, cb);
     }
 
-    function fetchDb(point,callback) {
-        console.log("Fetching from db with details "+JSON.stringify(point));
+    function fetchDb(point, callback) {
+        console.log("Fetching from db with details " + JSON.stringify(point));
         var cb = function (err, results) {
-            console.log("error occured in fetch" +err+ "     " +results );
-            if(results.length == 0){
+            if (err) {
+                console.log("Error occured during fetch: " + err);
+            }
+            if (results.length == 0) {
                 err = 400;
             }
             callback(err, results);
         }
-        pointDb.all("SELECT * from pointInfo WHERE point_id=?", point.point_id,cb);
+        pointDb.all("SELECT * from pointInfo WHERE point_id=?", point.point_id, cb);
     }
 
     function fetchAll(callback) {
         var cb = function (err, results) {
-           
-            if(results.length == 0){
-                console.log("error occured in fetch" +err+ "     " +results );
+
+            if (results.length == 0) {
+                console.log("error occured in fetch" + err + "     " + results);
                 err = 400;
             }
             callback(err, results);
         }
-        pointDb.all("SELECT * from pointInfo",cb);
+        pointDb.all("SELECT * from pointInfo", cb);
+    }
+
+    function removeAll(callback) {
+        console.log("Removing all data from database.");
+        var cb = function (err, results) {
+            if (err) {
+                console.log("Error occured while deleting database: " + err);
+            }
+            callback(err, results);
+        }
+        pointDb.all("DELETE from pointInfo", cb);
     }
 
     function closeDb(callback) {
-       
-
-             pointDb.close(); 
-            callback();
-       
+        pointDb.close();
+        callback();
     }
 
     return {
-        initializeDb : initializeDb,
-        updateDb : updateDb,
-        closeDb : closeDb,
-        fetchDb : fetchDb,
-        fetchAll : fetchAll
+        initializeDb: initializeDb,
+        updateDb: updateDb,
+        closeDb: closeDb,
+        fetchDb: fetchDb,
+        fetchAll: fetchAll,
+        removeAll: removeAll
     }
 
 }
--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       
+var bodyParser = require('body-parser');
+const { result } = require("lodash");
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
-})); 
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,51 +24,66 @@ db = new dbConn();
 db.initializeDb();
 
 var port = process.env.PORT || 8009;
-server.listen(port,function (err) {
-    console.log("Listening to port "+port);
+server.listen(port, function (err) {
+  console.log("Listening to port " + port);
 });
 
-app.get('/',function(req,res){
-    console.log("******************");
-    res.send('We are happy to see you');
-  });
-  app.post('/',function (req,res) {
-
-    console.log("****************** POST request ***********"+JSON.stringify(req));
-
+app.get('/', function (req, res) {
+  console.log("******************");
+  res.send('We are happy to see you');
 });
 
-app.get('/points',function(req,res){
+app.post('/', function (req, res) {
+  console.log("****************** POST request ***********" + JSON.stringify(req));
+});
+
+app.get('/cleandb', function(req, res) {
+  console.log("Delete all data request received");
+  db.removeAll((err, result) => {
+    console.log("Result: " + JSON.stringify(result));
+    if (err)
+    {
+      res.send("Failed to delete database.");
+    }
+    else
+    {
+      res.send("Database deletion successful.");
+    }
+  })
+});
+
+app.get('/points', function (req, res) {
   console.log("***********getallpoints*******");
-   db.fetchAll((err, result) => {
-      console.log("details"+JSON.stringify(result));
-      res.render('points', {
-        points: result
-      });
-    })      
+  db.fetchAll((err, result) => {
+    console.log("All details" + JSON.stringify(result));
+    res.render('points', {
+      points: result
+    });
+  })
 });
 
-app.get('/api/points',function(req,res){
+app.get('/api/points', function (req, res) {
   console.log("***********getallpoints*******");
-   db.fetchAll((err, result) => {
-      console.log("details"+JSON.stringify(result));
-      res.send(result);
-    })      
+  db.fetchAll((err, result) => {
+    console.log("details" + JSON.stringify(result));
+    res.send(result);
+  })
 });
 
-app.post('/injest',function (req,res) {
+app.post('/injest', function (req, res) {
 
-  console.log("****************** recieved body ***********"+JSON.stringify(req.body));
-  console.log("****************** recieved header ***********"+JSON.stringify(req.headers));
-  if(req.body.point && req.body.point) {
-  db.updateDb(req.body.point, () => {
-    db.fetchDb({"point_id": req.body.point.point_id}, (err, result) => {
-      console.log("details"+JSON.stringify(result));
-    })
-    res.send("updated");
-  });
-}
-else {
-  res.send("point not sent");
-}
+  console.log("****************** recieved body ***********" + JSON.stringify(req.body));
+  console.log("****************** recieved header ***********" + JSON.stringify(req.headers));
+  if (req.body.point) {
+    db.updateDb(req.body.point, () => {
+      db.fetchDb({ "point_id": req.body.point.point_id }, (err, result) => {
+        console.log("Point details: " + JSON.stringify(result));
+      })
+    });
+    res.redirect("/points");
+  }
+  else {
+    res.send("Point not sent");
+  }
+
 });
